6301f946e7ef91048fa0ca2a80d85651619b20d7
Duarte099 <a14395@oficina.pt>
2025-05-08 14:09:58 +0200

    Corrigido outro erro dos pagamentos atrasados

--- scriptPagamentosAtrasados.php
+++ scriptPagamentosAtrasados.php
@@ -17,12 +17,12 @@
     $ano = $dataAnterior->format('Y');
 
     // RECIBO ALUNOS
-    $sql1 = "SELECT alunos.id, estado FROM alunos INNER JOIN alunos_recibos ON alunos.id = idAluno WHERE ativo = 1 AND estado = 'Pendente' AND ano = $ano AND mes = $mes";
+    $sql1 = "SELECT alunos.id, estado FROM alunos INNER JOIN alunos_recibo ON alunos.id = idAluno WHERE ativo = 1 AND estado = 'Pendente' AND ano = $ano AND mes = $mes";
     $result1 = $con->query($sql1);
     if ($result1->num_rows > 0) {
         while ($row1 = $result1->fetch_assoc()) {
             // Inserir Pagamento com a data do mês anterior
-            $sql4 = "UPDATE alunos_recibos SET estado = 'Em atraso', coima = ? WHERE idAluno = ? AND ano = ? AND mes = ?";
+            $sql4 = "UPDATE alunos_recibo SET estado = 'Em atraso', coima = ? WHERE idAluno = ? AND ano = ? AND mes = ?";
             $result4 = $con->prepare($sql4);
             if ($result4) {
                 $result4->bind_param("iiii", $valorCoima, $row1['id'], $ano, $mes);
