f7d08bff82b86a6ff5793901187840762cdb739a
Duarte099 <a14395@oficina.pt>
2025-05-08 12:09:03 +0200

    2 tentativa de correção

--- scriptSeguro.php
+++ scriptSeguro.php
@@ -9,7 +9,7 @@
     $cronjob = true;
     include('/home/xpt123/admin/db/conexao.php');
 
-    require '/home/xpt123/cronJobs/PHPMailer/src/PHPMailer.php'; 
+    require '/home/xpt123/cronJobs/vendor/autoload.php';
 
     use Dompdf\Dompdf;
     use Dompdf\Options;
