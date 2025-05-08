1f0dfaa74685f76b72db6d4143737b809918ecfb
Duarte099 <a14395@oficina.pt>
2025-05-08 18:03:05 +0200

    Colocada função para poder desativar e ativar os cronjobs quando precisar e removido cronjob de pagamentos atrasados devido a não ser preciso

--- scriptNovoAnoLetivo.php
+++ scriptNovoAnoLetivo.php
@@ -3,21 +3,23 @@
     $cronjob = true;
     include('/home/xpt123/admin/db/conexao.php');
 
-    // INCREMENTAR NOVO ANO
-    $sql1 = "SELECT * FROM alunos WHERE ativo = 1 AND id = 156";
-    $result1 = $con->query($sql1);
-    if ($result1->num_rows > 0) {
-        while ($row1 = $result1->fetch_assoc()) {
-            // Inserir Pagamento com a data do mês anterior
-            $sql4 = "UPDATE alunos SET ano = ? WHERE id = ?";
-            $result4 = $con->prepare($sql4);
-            $ano = $row1["ano"] + 1;
-            if ($ano > 12) {
-                $ano = 0;
-            }
-            if ($result4) {
-                $result4->bind_param("ii", $ano, $row1['id']);
-                $result4->execute();
+    if ($cronjobNovoAnoLetivo == 1) {
+        // INCREMENTAR NOVO ANO
+        $sql1 = "SELECT * FROM alunos WHERE ativo = 1 AND id = 156";
+        $result1 = $con->query($sql1);
+        if ($result1->num_rows > 0) {
+            while ($row1 = $result1->fetch_assoc()) {
+                // Inserir Pagamento com a data do mês anterior
+                $sql4 = "UPDATE alunos SET ano = ? WHERE id = ?";
+                $result4 = $con->prepare($sql4);
+                $ano = $row1["ano"] + 1;
+                if ($ano > 12) {
+                    $ano = 0;
+                }
+                if ($result4) {
+                    $result4->bind_param("ii", $ano, $row1['id']);
+                    $result4->execute();
+                }
             }
         }
     }
--- scriptSeguro.php
+++ scriptSeguro.php
@@ -14,81 +14,82 @@
     use Dompdf\Dompdf;
     use Dompdf\Options;
 
-    /**
-     * Set the Dompdf options
-     */
-    $options = new Options;
-    $options->setChroot(__DIR__);
-    $options->setIsRemoteEnabled(true);
-
-    $dompdf = new Dompdf($options);
-
-    /**
-     * Set the paper size and orientation
-     */
-    $dompdf->setPaper("A4", "landscape");
-
-    /**
-     * Render the PHP template before passing it to Dompdf
-     */
-    ob_start();
-    require '/home/xpt123/cronJobs/scriptSeguroPdf.php';
-    $html = ob_get_clean();
-
-    $dompdf->loadHtml($html);
-
-    /**
-     * Create the PDF and set attributes
-     */
-    $dompdf->render();
-
-    $dompdf->addInfo("Title", "Lista Alunos Seguro");
-
-    /**
-     * Save the PDF file locally
-     */
-    $output = $dompdf->output();
-    file_put_contents("/home/xpt123/admin/uploads/seguro/listaAlunosSeguro_" . date("d-m-y") . ".pdf", $output);
-
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require '/home/xpt123/cronJobs/PHPMailer/src/Exception.php';
-    require '/home/xpt123/cronJobs/PHPMailer/src/PHPMailer.php';
-    require '/home/xpt123/cronJobs/PHPMailer/src/SMTP.php';
+    if ($cronjobSeguro == 1) {
+        /**
+         * Set the Dompdf options
+         */
+        $options = new Options;
+        $options->setChroot(__DIR__);
+        $options->setIsRemoteEnabled(true);
 
-    $mail = new PHPMailer(true);
+        $dompdf = new Dompdf($options);
 
-    $mail->isSMTP();
-    $mail->SMTPDebug  = 2;                     // enables SMTP debug information 
-    $mail->SMTPAuth   = true;                  // enable SMTP authentication
-    $mail->Port       = 587;
-    $mail->Host       = "mail.4x1.pt"; // sets the SMTP server                // set the SMTP port for the GMAIL server
-    $mail->Username   = "geral@4x1.pt"; // SMTP account username
-    $mail->Password   = "nTgY}w0_fBj}";        // SMTP account password     
+        /**
+         * Set the paper size and orientation
+         */
+        $dompdf->setPaper("A4", "landscape");
 
-    $mail->addAddress('duarte102007marques@gmail.com');
-    $mail->SetFrom('geral@4x1.pt', '4x1 | CENTRO DE ESTUDO');
-    $mail->isHTML(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->Subject = 'LISTA DOS ALUNOS';
+        /**
+         * Render the PHP template before passing it to Dompdf
+         */
+        ob_start();
+        require '/home/xpt123/cronJobs/scriptSeguroPdf.php';
+        $html = ob_get_clean();
 
-    $mail->Body = "
-            Boa Noite,
-            <p>Envio lista atualizada dos alunos que frequentam o Centro de Estudo 4x1.
-            <p>
-            <p>
-            CENTRO DE ESTUDO 4x1 | Alameda Arnaldo Gama nº 161, 4765-001 Vila das Aves | email: geral@4x1.pt
-            </body>";
+        $dompdf->loadHtml($html);
 
-    $pdfPath = "/home/xpt123/admin/uploads/seguro/listaAlunosSeguro_" . date("d-m-y") . ".pdf";
-    if (file_exists($pdfPath)) {
-        $mail->addAttachment($pdfPath, "LISTA_ALUNOS_" . date("mdy") . "pdf");
-    } else {
-        throw new Exception("O ficheiro PDF não foi encontrado: $pdfPath");
+        /**
+         * Create the PDF and set attributes
+         */
+        $dompdf->render();
+
+        $dompdf->addInfo("Title", "Lista Alunos Seguro");
+
+        /**
+         * Save the PDF file locally
+         */
+        $output = $dompdf->output();
+        file_put_contents("/home/xpt123/admin/uploads/seguro/listaAlunosSeguro_" . date("d-m-y") . ".pdf", $output);
+
+        require '/home/xpt123/cronJobs/PHPMailer/src/Exception.php';
+        require '/home/xpt123/cronJobs/PHPMailer/src/PHPMailer.php';
+        require '/home/xpt123/cronJobs/PHPMailer/src/SMTP.php';
+
+        $mail = new PHPMailer(true);
+
+        $mail->isSMTP();
+        $mail->SMTPDebug  = 2;                     // enables SMTP debug information 
+        $mail->SMTPAuth   = true;                  // enable SMTP authentication
+        $mail->Port       = 587;
+        $mail->Host       = "mail.4x1.pt"; // sets the SMTP server                // set the SMTP port for the GMAIL server
+        $mail->Username   = "geral@4x1.pt"; // SMTP account username
+        $mail->Password   = "nTgY}w0_fBj}";        // SMTP account password     
+
+        $mail->addAddress('marco.rodrigues@segup.pt');
+        $mail->SetFrom('geral@4x1.pt', '4x1 | CENTRO DE ESTUDO');
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = 'LISTA DOS ALUNOS';
+
+        $mail->Body = "
+                Boa Noite,
+                <p>Envio lista atualizada dos alunos que frequentam o Centro de Estudo 4x1.
+                <p>
+                <p>
+                CENTRO DE ESTUDO 4x1 | Alameda Arnaldo Gama nº 161, 4765-001 Vila das Aves | email: geral@4x1.pt
+                </body>";
+
+        $pdfPath = "/home/xpt123/admin/uploads/seguro/listaAlunosSeguro_" . date("d-m-y") . ".pdf";
+        if (file_exists($pdfPath)) {
+            $mail->addAttachment($pdfPath, "LISTA_ALUNOS_" . date("mdy") . "pdf");
+        } else {
+            throw new Exception("O ficheiro PDF não foi encontrado: $pdfPath");
+        }
+
+        $mail->send();
+        //$mail->AddReplyTo("geral@4x1.pt","4x1.pt");
     }
-
-    $mail->send();
-    //$mail->AddReplyTo("geral@4x1.pt","4x1.pt");
 ?>
-</html>
