d9a71fcaff7b17f8adf29c8ae7ee202e425f532c
Duarte099 <a14395@oficina.pt>
2025-05-08 10:23:32 +0200

    Apenas cronjobs, eliminado tudo o resto

--- indexLogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //Inclui a base de dados e segurança da pagina
-    include("./db/conexao.php");
-    //Destroi a sessão (logout)
-    session_destroy();
-    //Redirect to the login page:
-    header('Location: index.php');
-?>
