24f7fa520bbcf3a70ba29afb85ed56a09a3ebaf2
Duarte099 <a14395@oficina.pt>
2025-05-09 10:37:09 +0200

    Adicionada definição para ativar e desativar cronjob de despesas

--- scriptDespesas.php
+++ scriptDespesas.php
@@ -4,20 +4,22 @@
     $categoria = 1;
     include('/home/xpt123/admin/db/conexao.php');
 
-    $sql1 = "SELECT * FROM despesas";
-    $result1 = $con->query($sql1);
-    if ($result1->num_rows > 0) {
-        while ($row1 = $result1->fetch_assoc()) {
-            $sql2 = "SELECT * FROM despesas_meses WHERE idDespesa = {$row1['id']}";
-            $result2 = $con->query($sql2);
-            if ($result2->num_rows > 0) {
-                while ($row2 = $result2->fetch_assoc()) {
-                    if ($row2['mes'] == date('n')) {
-                        $sql3 = "INSERT INTO `transacoes` (`idCategoria`, `descricao`, `valor`) VALUES (?, ?, ?);";
-                        $result3 = $con->prepare($sql3);
-                        if ($result3) {
-                            $result3->bind_param("iss", $categoria, $row1['despesa'], $row1['valor']);
-                            $result3->execute();
+    if ($cronjobDespesas == 1) {
+        $sql1 = "SELECT * FROM despesas";
+        $result1 = $con->query($sql1);
+        if ($result1->num_rows > 0) {
+            while ($row1 = $result1->fetch_assoc()) {
+                $sql2 = "SELECT * FROM despesas_meses WHERE idDespesa = {$row1['id']}";
+                $result2 = $con->query($sql2);
+                if ($result2->num_rows > 0) {
+                    while ($row2 = $result2->fetch_assoc()) {
+                        if ($row2['mes'] == date('n')) {
+                            $sql3 = "INSERT INTO `transacoes` (`idCategoria`, `descricao`, `valor`) VALUES (?, ?, ?);";
+                            $result3 = $con->prepare($sql3);
+                            if ($result3) {
+                                $result3->bind_param("iss", $categoria, $row1['despesa'], $row1['valor']);
+                                $result3->execute();
+                            }
                         }
                     }
                 }
