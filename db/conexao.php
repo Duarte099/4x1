6f3cebaa996b96cdb0450ba249d297e3fccd3fa0
Duarte099 <a14395@oficina.pt>
2025-05-27 14:57:12 +0200

    Correção do caminho das logs

--- db/conexao.php
+++ db/conexao.php
@@ -3,7 +3,7 @@
 
     ini_set('display_errors', 0);
     ini_set('log_errors', 1);
-    ini_set('error_log', '/home/xpt123/admin/db/PHP.log');
+    ini_set('error_log', '/home/xpt123/logs/PHP.log');
     error_reporting(E_ALL);
 
     // Configurações de conexão ao banco de dados
