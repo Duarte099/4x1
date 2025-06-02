1ec6b358feeae41774d745d8f6b7e1b1cc5b31ae
Duarte099 <a14395@oficina.pt>
2025-06-02 16:04:12 +0100

    Corrigido erro no cronjob de incrementar o ano no fim do ano letivo

--- scriptNovoAnoLetivo.php
+++ scriptNovoAnoLetivo.php
@@ -9,15 +9,16 @@
         $result1 = $con->query($sql1);
         if ($result1->num_rows > 0) {
             while ($row1 = $result1->fetch_assoc()) {
-                // Inserir Pagamento com a data do mês anterior
+                if ($row1["ano"] == 12 || $row1["ano"] == 0) {
+                    $row1["ano"] = 0;
+                }
+                else {
+                    $row1["ano"]++;
+                }
                 $sql4 = "UPDATE alunos SET ano = ? WHERE id = ?";
                 $result4 = $con->prepare($sql4);
-                $ano = $row1["ano"] + 1;
-                if ($ano > 12) {
-                    $ano = 0;
-                }
                 if ($result4) {
-                    $result4->bind_param("ii", $ano, $row1['id']);
+                    $result4->bind_param("ii", $row1["ano"], $row1['id']);
                     $result4->execute();
                 }
             }
