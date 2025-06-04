e6978eb956c145536373dfdce28e380bcfbd4212
Duarte099 <a14395@oficina.pt>
2025-06-04 16:37:37 +0100

    Correção do titulo da pagina de login

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login | 4x1</title>
+    <title>4x1 | Login</title>
     <link rel="icon" href="./images/Icon4x1.png" type="image/x-icon"/>
 
     <!-- Font Icon -->
