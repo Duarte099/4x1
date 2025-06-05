f2dee4480dcb40f10568a4a6fe1b5591ac274fed
Duarte099 <a14395@oficina.pt>
2025-06-05 18:15:40 +0100

    Notificações para os alunos que os recibos ja foram verificados

--- aluno.php
+++ aluno.php
@@ -69,11 +69,11 @@
                         <tbody>
                           <?php
                             //query para selecionar todos os administradores
-                            $sql = "SELECT id, nome, ano, dataNascimento, horasGrupo, horasIndividual, IF(ano>=1 AND ano<=4, \"1º CICLO\", IF(ano>4 AND ano<7, \"2º CICLO\", IF(ano>6 AND ano<=9, \"3º CICLO\", IF(ano>9 AND ano<=12, \"SECUNDÁRIO\", IF(ano=0, \"UNIVERSIDADE\", \"ERRO\"))))) as ensino FROM alunos WHERE ativo = 1 ORDER BY (ano = 0), ano ASC;";
+                            $sql = "SELECT id, nome, ano, DATE_FORMAT(dataNascimento, '%d-%m-%Y') as dataNascimento, horasGrupo, horasIndividual, IF(ano>=1 AND ano<=4, \"1º CICLO\", IF(ano>4 AND ano<7, \"2º CICLO\", IF(ano>6 AND ano<=9, \"3º CICLO\", IF(ano>9 AND ano<=12, \"SECUNDÁRIO\", IF(ano=0, \"UNIVERSIDADE\", \"ERRO\"))))) as ensino FROM alunos WHERE ativo = 1 ORDER BY (ano = 0), ano ASC;";
                             $result = $con->query($sql);
                             if ($result->num_rows > 0) {
                               while ($row = $result->fetch_assoc()) {
-                                //mostra os resultados todos 
+                                //mostra os resultados todos
                                 echo "<tr>
                                         <td>{$row['ensino']}</td>
                                         <td>{$row['nome']}</td>
