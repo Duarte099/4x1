aaf4db16563cbede0333c4807cc37f09d708c11d
Duarte099 <a14395@oficina.pt>
2025-06-06 14:07:08 +0100

    correção de erro

--- aluno.php
+++ aluno.php
@@ -73,11 +73,11 @@
                                 $result = $con->query($sql);
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) { 
-                                            if ($row['estado'] == 1) {
-                                                $row['estado'] = "Ativo";
+                                            if ($row['ativo'] == 1) {
+                                                $row['ativo'] = "Ativo";
                                             }
                                             else {
-                                                $row['estado'] = "Inativo";
+                                                $row['ativo'] = "Inativo";
                                             }
                                         ?>
                                         <tr>
