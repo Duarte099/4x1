15fceea786492d0016e8d554f5f391a30c1b92ae
Duarte099 <a14395@oficina.pt>
2025-06-06 14:05:21 +0100

    Correção de erro

--- aluno.php
+++ aluno.php
@@ -69,7 +69,7 @@
                         <tbody>
                             <?php
                                 //query para selecionar todos os administradores
-                                $sql = "SELECT id, nome, ano, DATE_FORMAT(dataNascimento, '%d-%m-%Y') as dataNascimento, horasGrupo, horasIndividual, IF(ano>=1 AND ano<=4, \"1º CICLO\", IF(ano>4 AND ano<7, \"2º CICLO\", IF(ano>6 AND ano<=9, \"3º CICLO\", IF(ano>9 AND ano<=12, \"SECUNDÁRIO\", IF(ano=0, \"UNIVERSIDADE\", \"ERRO\"))))) as ensino FROM alunos ORDER BY (ano = 0), ano ASC, ativo DESC;";
+                                $sql = "SELECT id, nome, ano, DATE_FORMAT(dataNascimento, '%d-%m-%Y') as dataNascimento, horasGrupo, horasIndividual, IF(ano>=1 AND ano<=4, \"1º CICLO\", IF(ano>4 AND ano<7, \"2º CICLO\", IF(ano>6 AND ano<=9, \"3º CICLO\", IF(ano>9 AND ano<=12, \"SECUNDÁRIO\", IF(ano=0, \"UNIVERSIDADE\", \"ERRO\"))))) as ensino, ativo FROM alunos ORDER BY (ano = 0), ano ASC, ativo DESC;";
                                 $result = $con->query($sql);
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) { 
@@ -84,7 +84,7 @@
                                             <td><?php echo $row['ensino'] ?></td>
                                             <td><?php echo $row['nome'] ?></td>
                                             <td><?php echo $row['dataNascimento'] ?></td>
-                                            <td><?php echo $row['estado'] ?></td>
+                                            <td><?php echo $row['ativo'] ?></td>
                                             <td>
                                                 <div class="form-button-action">
                                                     <button
