4831f88aaf762711b69f2a5803f49376e8c09f58
Duarte099 <a14395@oficina.pt>
2025-06-09 11:43:18 +0100

    correção de erro

--- aluno.php
+++ aluno.php
@@ -138,37 +138,39 @@
                 { targets: 4, orderable: false }
             ],
             initComplete: function () {
-                this.api().column(3).search('Ativo').draw();
-                this.api()
+            this.api()
                 .columns()
                 .every(function () {
                     var column = this;
                     var select = $(
                         '<select class="form-select"><option value=""></option></select>'
                     )
-                    .appendTo($(column.footer()).empty())
-                    .on("change", function () {
-                        var val = $.fn.dataTable.util.escapeRegex($(this).val());
-
-                        column
-                        .search(val ? "^" + val + "$" : "", true, false)
-                        .draw();
-                    });
+                        .appendTo($(column.footer()).empty())
+                        .on("change", function () {
+                            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                            column
+                                .search(val ? "^" + val + "$" : "", true, false)
+                                .draw();
+                        });
 
                     column
-                    .data()
-                    .unique()
-                    .sort()
-                    .each(function (d, j) {
-                        select.append(
-                            '<option value="' + d + '">' + d + "</option>"
-                        );
-                        if (column.index() === 3) {
-                            select.val('Ativo');
-                        }
-                    });
+                        .data()
+                        .unique()
+                        .sort()
+                        .each(function (d, j) {
+                            select.append(
+                                '<option value="' + d + '">' + d + "</option>"
+                            );
+                        });
+
+                    if (column.index() === 3) {
+                        select.val('Ativo');
+                    }
                 });
-            },
+
+            // Só depois de criados os selects, aplicar o filtro "Ativo"
+            this.api().column(3).search('^Ativo$', true, false).draw();
+        }
         });
     </script>
   </body>
