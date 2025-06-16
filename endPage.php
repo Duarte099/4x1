d15e98994ed1318326df735063d88ad612a0c197
Duarte099 <a14395@oficina.pt>
2025-06-16 12:07:40 +0100

    teste

--- endPage.php
+++ endPage.php
@@ -8,12 +8,10 @@
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
 <script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
 <script src="assets/js/plugin/chart-circle/circles.min.js"></script>
-<script src="assets/js/plugin/datatables/datatables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"></script>
 <script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
 <script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
-<script src="assets/js/kaiadmin.min.js"></script>
 
 <!-- Service Worker -->
 <script>
