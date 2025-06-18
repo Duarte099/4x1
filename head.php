dfe28f68498825233894abb7a1a9f18ebd5fd4a8
Duarte099 <a14395@oficina.pt>
2025-06-18 11:32:58 +0100

    teste

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 <?php 
-    //inclui a base de dados e segurança da página
+    // Inclui a base de dados e segurança da página
     include('./db/conexao.php'); 
 ?>
 
@@ -9,50 +9,37 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta
-        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
-        name="viewport"
-    />
-    <link
-        rel="icon"
-        href="./images/Icon4x1.png"
-        type="image/x-icon"
-    />
-    <link rel="manifest" href="/manifest.json">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <link rel="icon" href="./images/Icon4x1.png" type="image/x-icon" />
+
     <!-- Estilos do intl-tel-input -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css"/>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    
+    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Fonts and icons -->
+    <!-- Fonts and Icons -->
     <script src="assets/js/plugin/webfont/webfont.min.js"></script>
     <script>
         WebFont.load({
-        google: { families: ["Public Sans:300,400,500,600,700"] },
-        custom: {
-            families: [
-            "Font Awesome 5 Solid",
-            "Font Awesome 5 Regular",
-            "Font Awesome 5 Brands",
-            "simple-line-icons",
-            ],
-            urls: ["assets/css/fonts.min.css"],
-        },
-        active: function () {
-            sessionStorage.fonts = true;
-        },
+            google: { families: ["Public Sans:300,400,500,600,700"] },
+            custom: {
+                families: [
+                    "Font Awesome 5 Solid",
+                    "Font Awesome 5 Regular",
+                    "Font Awesome 5 Brands",
+                    "simple-line-icons",
+                ],
+                urls: ["assets/css/fonts.min.css"],
+            },
+            active: function () {
+                sessionStorage.fonts = true;
+            },
         });
     </script>
 
-    <script>
-        if ("serviceWorker" in navigator) {
-            navigator.serviceWorker
-                .register("/sw.js")
-                .catch(() => {});
-        }
-    </script>
-
+    <!-- Notificação PHP -->
     <script>
         $(document).ready(function () {
             <?php if (isset($_SESSION['notificacao'])): ?>
@@ -62,14 +49,10 @@
                     icon: 'fa fa-info-circle',
                 }, {
                     type: '<?php echo $_SESSION['notificacao']['tipo']; ?>',
-                    placement: {
-                        from: 'top',
-                        align: 'right'
-                    },
+                    placement: { from: 'top', align: 'right' },
                     delay: 2000
                 });
-
-                <?php unset($_SESSION['notificacao']); // Limpa a notificação após exibir ?>
+                <?php unset($_SESSION['notificacao']); ?>
             <?php endif; ?>
         });
     </script>
@@ -79,56 +62,32 @@
     <link rel="stylesheet" href="assets/css/plugins.min.css" />
     <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
 
-    <!--   Core JS Files   -->
+    <!-- Scripts -->
+    <!-- Core JS Files -->
     <script src="assets/js/core/popper.min.js"></script>
     <script src="assets/js/core/bootstrap.min.js"></script>
 
     <!-- jQuery Scrollbar -->
     <script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
 
-    <!-- Chart JS -->
-    <script src="assets/js/plugin/chart.js/chart.min.js"></script>
+    <!-- Chart.js e plugins -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
 
-    <!-- jQuery Sparkline -->
+    <!-- Sparkline -->
     <script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
 
     <!-- Chart Circle -->
     <script src="assets/js/plugin/chart-circle/circles.min.js"></script>
 
-    <!-- Datatables -->
+    <!-- DataTables -->
     <script src="assets/js/plugin/datatables/datatables.min.js"></script>
 
     <!-- Bootstrap Notify -->
     <script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
 
-    <!-- jQuery Vector Maps -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
-
     <!-- Sweet Alert -->
     <script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
-    <!-- Kaiadmin JS -->
+    <!-- Kaiadmin -->
     <script src="assets/js/kaiadmin.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            <?php if (isset($_GET['erro']) && $_GET['erro'] == "true"): ?>
-                $.notify({
-                    message: 'Password ou user Incorreto!',
-                    title: 'Notificação',
-                    icon: 'fa fa-info-circle',
-                }, {
-                    type: 'danger',
-                    placement: {
-                        from: 'top',
-                        align: 'right'
-                    },
-                    delay: 2000
-                });
-                const cleanUrl = window.location.origin + window.location.pathname;
-                window.history.replaceState({}, document.title, cleanUrl);
-            <?php endif; ?>
-        });    
-        
-    </script>
