d57748f2129b5cf80ac058627d23ab5fcc50a0fe
Duarte099 <a14395@oficina.pt>
2025-06-18 09:12:43 +0100

    teste

--- head.php
+++ head.php
@@ -12,33 +12,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <link rel="icon" href="./images/Icon4x1.png" type="image/x-icon" />
 
-    <!-- SEO & Redes Sociais -->
-    <meta property="og:title" content="Centro de Estudo 4x1">
-    <meta property="og:description" content="Explicações, apoio escolar e muito mais.">
-    <meta property="og:image" content="https://admin.4x1.pt/images/LogoBranco4x1.png">
-    <meta property="og:url" content="https://admin.4x1.pt">
-
-    <!-- Web App iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <meta name="apple-mobile-web-app-title" content="4x1 Centro de Estudo">
-
-    <!-- Ícones & Manifest -->
-    <link rel="apple-touch-icon" href="/images/icon4x1_192x192.png">
-    <link rel="apple-touch-startup-image" href="/images/icon4x1_512x512.png">
-    <link rel="manifest" href="/manifest.json">
-
-    <!-- CSS Files -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="assets/css/plugins.min.css" />
-    <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
+    <!-- Estilos do intl-tel-input -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-
-    <!-- jQuery (necessário antes dos plugins) -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Fonts and Icons -->
+    <!-- Fonts and icons -->
     <script src="assets/js/plugin/webfont/webfont.min.js"></script>
     <script>
         WebFont.load({
@@ -58,22 +37,30 @@
         });
     </script>
 
-    <!-- Bootstrap Core -->
-    <script src="assets/js/core/popper.min.js"></script>
-    <script src="assets/js/core/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            <?php if (isset($_SESSION['notificacao'])): ?>
+                $.notify({
+                    message: '<?php echo addslashes($_SESSION['notificacao']['mensagem']); ?>',
+                    title: 'Notificação',
+                    icon: 'fa fa-info-circle',
+                }, {
+                    type: '<?php echo $_SESSION['notificacao']['tipo']; ?>',
+                    placement: {
+                        from: 'top',
+                        align: 'right'
+                    },
+                    delay: 2000
+                });
+                <?php unset($_SESSION['notificacao']); ?>
+            <?php endif; ?>
+        });
+    </script>
 
-    <!-- Plugins JS -->
-    <script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
-    <script src="assets/js/plugin/datatables/datatables.min.js"></script>
-    <script src="assets/js/plugin/chart.js/chart.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
-    <script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
-    <script src="assets/js/plugin/chart-circle/circles.min.js"></script>
-    <script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
-    <script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
-
-    <!-- Kaiadmin Template JS -->
-    <script src="assets/js/kaiadmin.min.js"></script>
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins.min.css" />
+    <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
 
     <!-- Service Worker -->
     <script>
@@ -84,7 +71,6 @@
         }
     </script>
 
-    <!-- Swipe para abrir o menu lateral -->
     <script>
         let touchStartX = 0;
         let touchEndX = 0;
@@ -111,25 +97,33 @@
         }
     </script>
 
-    <!-- Notificações com Bootstrap Notify -->
-    <script>
-        $(document).ready(function () {
-            <?php if (isset($_SESSION['notificacao'])): ?>
-                $.notify({
-                    message: '<?php echo addslashes($_SESSION['notificacao']['mensagem']); ?>',
-                    title: 'Notificação',
-                    icon: 'fa fa-info-circle',
-                }, {
-                    type: '<?php echo $_SESSION['notificacao']['tipo']; ?>',
-                    placement: {
-                        from: 'top',
-                        align: 'right'
-                    },
-                    delay: 2000
-                });
-                <?php unset($_SESSION['notificacao']); ?>
-            <?php endif; ?>
-        });
-    </script>
+    <!-- Core JS Files -->
+    <script src="assets/js/core/popper.min.js"></script>
+    <script src="assets/js/core/bootstrap.min.js"></script>
+
+    <!-- jQuery Scrollbar -->
+    <script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+
+    <!-- Chart JS -->
+    <script src="assets/js/plugin/chart.js/chart.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
+
+    <!-- jQuery Sparkline -->
+    <script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Chart Circle -->
+    <script src="assets/js/plugin/chart-circle/circles.min.js"></script>
+
+    <!-- Datatables -->
+    <script src="assets/js/plugin/datatables/datatables.min.js"></script>
+
+    <!-- Bootstrap Notify -->
+    <script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+
+    <!-- Sweet Alert -->
+    <script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+
+    <!-- Kaiadmin JS -->
+    <script src="assets/js/kaiadmin.min.js"></script>
 </head>
 <body>
