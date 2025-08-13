28bbed8122e02a2e608afe77348254eddb127ba4
Duarte099 <a14395@oficina.pt>
2025-08-13 22:53:38 +0100

    Atualiza parâmetro de URL em alunoEdit.php para redirecionamento correto de recibos
    - Modifica o link de redirecionamento para utilizar 'id' em vez de 'idRecibo', assegurando que a URL aponte para o ID correto do recibo.

--- alunoEdit.php
+++ alunoEdit.php
@@ -543,7 +543,7 @@
                                                                                     </a>
                                                                                 <?php } else if ($row['notificacao'] == "Pendente") { ?>
                                                                                     <a
-                                                                                        href="recibosNotificacao?idRecibo=<?php echo $row['idRecibo']; ?>"
+                                                                                        href="recibosNotificacao?idRecibo=<?php echo $row['id']; ?>"
                                                                                         class="btn btn-link btn-primary btn-lg"
                                                                                         data-bs-toggle="tooltip"
                                                                                         data-bs-placement="top"
